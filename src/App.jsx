c46176142bb893544bff1fbbbcfda0c2e92b8779
Abduworabe <abdilkerimjuhar@gmail.com>
2025-04-16 08:14:05 -0800

    Add some props by pass

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,39 @@ import './App.css'
 import ExpenseItem from './consposnents/ExpenseItem'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const datab = [
+    {
+      id: 1,
+      title: 'somee',
+      price: 348,
+      date: new Date(2025, 2, 23),
+    },
+    {
+      id: 2,
+      title: 'HOnber',
+      price: 930.3,
+      date: new Date(2025, 2, 23),
+    },
+    {
+      id: 3,
+      title: 'mare',
+      price: 23,
+      date: new Date(2025, 2, 23),
+    },
+    {
+      id: 4,
+      title: 'chdoo',
+      price: 64.4,
+      date: new Date(2025, 2, 23),
+    },
+  ]
 
   return (
     <>
-<ExpenseItem/>
+      <ExpenseItem tiltle={datab[0].title} price={datab[0].price} date={datab[0].date} />
+      <ExpenseItem tiltle={datab[1].title} price={datab[1].price} date={datab[1].date} />
+      <ExpenseItem tiltle={datab[2].title} price={datab[2].price} date={datab[2].date} />
+      <ExpenseItem tiltle={datab[3].title} price={datab[3].price} date={datab[3].date} />
     </>
   )
 }
--- src/consposnents/ExpenseItem.jsx
+++ src/consposnents/ExpenseItem.jsx
@@ -1,17 +1,18 @@
 import classExpCss from './ExpenseItem.module.css'
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
 
+ 
     return <>
         <div className={classExpCss['expense-item']}>
             <div className={classExpCss['expense-itme-div1']}>
-               <div> March 28th 2025</div>
-                <h2 className={classExpCss['typeh2']}>Car_Insurance</h2>
+               <div> {props.date.getMonth()}</div>
+                <h2 className={classExpCss['typeh2']}>{props.tiltle}</h2>
                 
             </div>
         
                 <div  className={classExpCss['price']}>
 
-                    &23.33
+                    &{props.price}
                 </div>
             
         </div>
