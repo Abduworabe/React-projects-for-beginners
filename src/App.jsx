f7566a103318e17ed0334398a6cbf27d46cbb7ef
Abduworabe <abdilkerimjuhar@gmail.com>
2025-04-17 16:59:47 -0800

    Organizing Component files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ExpenseItem from './consposnents/ExpenseItem'
+import ExpenseItem from './consposnents/Expense/ExpenseItem'
 
 function App() {
   const datab = [
